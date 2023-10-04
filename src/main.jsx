9286dfa4a797a4a42dc57ca2af6858affef3bab6
Michael Dwyer <michael.dwyer@codeimmersives.com>
2023-10-04 12:11:21 -0700

    initial mui and react router scaffolding complete

--- src/main.jsx
+++ src/main.jsx
@@ -3,8 +3,17 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
+//Material UI
+import { ThemeProvider } from '@emotion/react'
+import {lightTheme, darkTheme} from './theme.jsx'
+import {CssBaseline} from '@mui/material'
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={darkTheme}>
+      {/* CssBaseline for browser compatibility */}
+      <CssBaseline />
+        <App />
+    </ThemeProvider>
   </React.StrictMode>,
 )
