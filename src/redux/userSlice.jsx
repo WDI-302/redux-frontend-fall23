8e3ea40627db29d6207c4de53096b2930744a396
Michael Dwyer <michael.dwyer@codeimmersives.com>
2023-10-05 11:02:02 -0700

    created first async thunk for logintest

--- src/redux/userSlice.jsx
+++ src/redux/userSlice.jsx
@@ -1,16 +1,45 @@
-import { createSlice } from '@reduxjs/toolkit'
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit'
+import Axios from '../lib/Axios'
+//thunk middleware is for async
+// createAsyncThunk, first parameter is your action.type (create is with the name + / + name of function)
+// nest param is the function, it takes in the payload data from the dispatch
+export const loginTest = createAsyncThunk('user/loginTest', async () => {
+    // call the backend / API
+    let response = await Axios.get('/users/login-test')
+
+    // return sets the action.payload for the extraReducers
+    // return {
+        // payload: response.data
+    // }
+    // essentially this breaks down to
+    // dispatch({
+    //     type: 'user/loginTest',
+    //     payload: response.data
+    // })
+
+    return response.data
+})
 
 export const userSlice = createSlice({
     name: 'user',
     initialState: {
-        firstname: 'Paul'
+        firstname: '',
+        lastname: '',
+        email: ''
     },
     //syncronous set state
     reducers: {
+        addUser: (state, action) => {}
 
+    },
+    // asyncronous set state
+    extraReducers: builder => {
+        builder.addCase(loginTest.fulfilled, (state, action) => {
+            state = action.payload
+        })
     }
 })
 
-export const {} = userSlice.actions
+export const { addUser } = userSlice.actions
 
 export default userSlice.reducer
